33245021c7e7302ea51175eac31a727308fc6936
Donovan Jasper <djasper@stanford.edu>
2024-07-10 11:35:46 -0700

    add {} to eliminate error

--- DomainPasswordSpray.ps1
+++ DomainPasswordSpray.ps1
@@ -257,8 +257,8 @@ function Countdown-Timer
         [switch] $Quiet = $False
     )
     if ($quiet)
-    {
-        Write-Host "$Message: Waiting for $($Seconds/60) minutes. $($Seconds - $Count)"
+  
+        Write-Host "${Message}: Waiting for $($Seconds/60) minutes. $($Seconds - $Count)"
         Start-Sleep -Seconds $Seconds
     } else {
         foreach ($Count in (1..$Seconds))
